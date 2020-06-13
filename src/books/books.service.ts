4dc04ed6a4ad329c14ab3a9cd63fee0d689e8db3
jeevantk <jeevan.thomaskoshy@gmail.com>
2020-06-13 13:08:41 +0530

    Initial draft

---
+++ src/books/books.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import {BOOKS} from '../mocks/book.mock'
+
+@Injectable()
+export class BooksService {
+    books = BOOKS;
+
+    getBooks(): Promise<any> {
+        return new Promise(resolve => {
+            resolve(this.books);
+        });
+    }
+
+    getBook(bookId:string|number): Promise<any> {
+        const id = Number(bookId);
+        return new Promise(resolve => {
+            const book =  this.books.find(book => book.id === id);
+            if(!book) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            resolve(book);
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    addBook(book:any): Promise<any> {
+        return new Promise(resolve => {
+            this.books.push(book);
+            resolve(this.books);
+        })
+    }
+
+    deleteBook(bookId:string|number): Promise<any> {
+        const id = Number(bookId);
+        return new Promise(resolve => {
+            const index = this.books.findIndex(book => book.id === id);
+            if(index === -1) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            this.books.splice(index, 1);
+            resolve(this.books);
+        })
+    }
+}
